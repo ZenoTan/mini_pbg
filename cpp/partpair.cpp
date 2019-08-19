f621fccfd03c466c247eb307c4773f71cec450cc
Tan <zeyut@a483e705d479.ant.amazon.com>
2019-08-19 18:09:21 +0800

    Partition pair processing

---
+++ cpp/partpair.cpp
@@ -0,0 +1,47 @@
+#include "partpair.h"
+
+namespace part {
+	void PartPair::Process(const string &data_file_name) {
+		ifstream file;
+		file.open(data_file_name);
+		int src, dst, rel, src_part, dst_part;
+		string first_line;
+		file >> first_line;
+		for (int i = 0; i < num_node; i++) {
+			file >> src;
+			file >> dst;
+			file >> rel;
+			src_part = partmap.GetPart(src);
+			dst_part = partmap.GetPart(dst);
+			pair<int, int> p;
+			if (src_part <= dst_part) {
+				p = {src_part, dst_part};
+			} else {
+				p = {dst_part, src_part};
+			}
+			if (edges.find(p) == edges.end()) {
+				edges[p] = {};
+			}
+			vector<int> edge{src, dst, rel};
+			edges[p].push_back(edge);
+		}
+		file.close();
+	}
+	void PartPair::OutputPair(pair<int, int> p, const string &file_name) {
+		ofstream file;
+		file.open(file_name);
+		for (auto &edge: edges[p]) {
+			file << edge[0] << ' ';
+			file << edge[1] << ' ';
+			file << edge[2] << endl;
+		}
+		file.close();
+	}
+}
+
+int main() {
+	part::PartPair partpair(86054151, 4, "dataset.txt.part.4");
+	partpair.Process("triple2id.txt");
+	partpair.OutputPair(make_pair<int, int>(0, 0), "0-0.txt");
+	return 0;
+}
---
+++ cpp/partpair.h
@@ -0,0 +1,23 @@
+#ifndef PARTPAIR_H
+#define PARTPAIR_H
+#include "partmap.h"
+#include <map>
+
+namespace part {
+class PartPair {
+public:
+	PartPair(int N, int P, const string &part_file_name): num_node(N), num_part(P), partmap(N, P, part_file_name) {
+		
+	}
+	void Process(const string &data_file_name);
+	void OutputPair(pair<int, int> p, const string &file_name);
+
+private:
+	const int num_node;
+	const int num_part;
+	PartMap partmap;
+	// vector<unordered_map<int, int>> id_map;
+	map<pair<int, int>, vector<vector<int>>> edges;
+};
+}
+#endif
