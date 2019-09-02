10cd56f18982ffb45509ac573f429ae5be4d907d
Ubuntu <ubuntu@ip-172-31-45-158.us-east-2.compute.internal>
2019-09-02 01:50:48 +0000

    Prepare for single machine test

--- python/main.py
+++ python/main.py
@@ -11,10 +11,10 @@ def test_train():
 	head_operator = ComplExOperator(14824, 400)
 	tail_operator = ComplExOperator(14824, 400)
 	comparator = DotComparator()
-	model_config = ModelConfig(head_operator, tail_operator, comparator, 1305371, 14824, 400, 1, 1000, 1000, 'Adagrad')
+	model_config = ModelConfig(head_operator, tail_operator, comparator, 1305371, 14824, 400, 8, 128, 256, 'Adagrad')
 	# model = Model(config)
 	loss_func = SoftmaxLoss(1000)
-	train_config = TrainConfig('part0.txt', 'head-tail-rel', 80000, model_config, 4, 1, loss_func)
+	train_config = TrainConfig('part0.txt', 'head-tail-rel', 192 * 1024,  model_config, 16, 1, loss_func)
 	trainer = Trainer(train_config)
 	t0 = time.time()
 	trainer.train()
@@ -102,4 +102,4 @@ def test_kv():
 	print(t1 - t0)
 
 if __name__ == '__main__':
-	test_kv()
+	test_train()
